812ae8dca9045e5d05b86edf5f371aa0c4e7f683
Dimitrios Kirtsios <dimkirts@gmail.com>
2019-12-22 15:46:51 +0100

    feat: Add book dto

--- src/wookie/books/resources.py
+++ src/wookie/books/resources.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
+from flask import request
 
 
 class BaseResource(Resource):
@@ -7,28 +8,47 @@ class BaseResource(Resource):
         self.dao = kwargs['dao']
 
 
+getBookDto = {
+    'id': fields.String,
+    'title': fields.String,
+    'author': fields.String,
+    'description': fields.String,
+    'price': fields.Float,
+    'cover': fields.String,
+    'links': {
+        'self': fields.FormattedString('{host}books/{id}')
+    }
+}
+
+
 class Books(BaseResource):
     """
     Resource for all the books
     """
+    @marshal_with(getBookDto, envelope='data')
     def get(self):
         """
         Return a JSON array of all books
         """
         self.logger.info('GET /books')
         books = self.dao.get_all_books()
-        return {'count': len(books), 'items': books}, 200
+        for book in books:
+            book['host'] = request.host_url
+
+        return books, 200
 
 
 class Book(BaseResource):
     """
     Resource for one book
     """
+    @marshal_with(getBookDto, envelope='data')
     def get(self, book_id):
         """
         Get a book by id
         """
         self.logger.info('GET /books/${}'.format(book_id))
-
+        print(request)
         book = self.dao.find_book_by_id(book_id)
+        book['host'] = request.host_url
         return book, 200
