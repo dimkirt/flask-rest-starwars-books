b5249a3ec213e4d9a90cef9cd9ffa995384f81dc
Dimitrios Kirtsios <dimkirts@gmail.com>
2019-12-26 14:02:18 +0100

    chore: Remove fake db from app code

--- src/wookie/wsgi.py
+++ src/wookie/wsgi.py
@@ -1,26 +1,6 @@
 from . import app
 
-# Use in-memory storage for now
-db = {
-    'books': [{
-        'id': 0,
-        'title': 'This is the title',
-        'author': 'This is the author',
-        'description': 'This is the description',
-        'price': 100,
-        'cover':
-        'https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/220px-Lenna_%28test_image%29.png',  # noqa
-        'publisher': 666,  # id of the publisher
-    }],
-    'users': [{
-        'id': 0,
-        'username': 'jedi-master',
-        'password': 'Test1234',
-        'author_pseudonym': 'Luke'
-    }]
-}
-
-prod_app = app.create_app(db)
+prod_app = app.create_app()
 
 if __name__ == '__main__':
     prod_app.run()
