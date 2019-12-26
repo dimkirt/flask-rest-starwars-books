4f7835f93b8ea5097e566154803deccfb942ab38
Dimitrios Kirtsios <dimkirts@gmail.com>
2019-12-26 14:56:38 +0100

    test: Add test to get books a user owns

--- test/integration/test_books.py
+++ test/integration/test_books.py
@@ -1,4 +1,5 @@
 import pytest  # noqa
+import json
 
 
 def test_get_all_books(test_client_with_db):
@@ -12,7 +13,7 @@ def test_get_all_books(test_client_with_db):
     assert resp.status_code == 200
     assert isinstance(resp.json['data'], list)
 
-    assert len(list(resp.json['data'])) == 2
+    assert len(resp.json['data']) == 2
     assert resp.json['data'][0]['id'] == '1'
     assert resp.json['data'][0]['title'] == 'The Wookiee Storybook'
     assert resp.json['data'][0][
@@ -89,3 +90,86 @@ def test_search_existing_book_by_title(test_client_with_db):
     assert 'publisher' not in resp.json['data']
     assert 'links' in resp.json['data']
     assert resp.json['data']['links']['self'] == 'http://localhost/books/1'
+
+
+def test_get_all_user_books_for_a_user_with_books(test_client_with_db):
+    """
+    GET /users/books
+    Get all books an authenticated user owns, in this case the user has books
+    """
+    # first with authenticate the user
+    url = '/auth'
+    mimetype = 'application/json'
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    data = {'username': 'chewbie', 'password': 'Test1234'}
+    resp = test_client_with_db.post(url,
+                                    data=json.dumps(data),
+                                    headers=headers)
+
+    assert resp.content_type == mimetype
+    assert resp.status_code == 200
+    assert isinstance(resp.json['data'], dict)
+    assert 'access_token' in resp.json['data']
+    access_token = resp.json['data']['access_token']
+
+    # then get the books the authenticated user owns
+    headers = {'Authorization': 'Bearer ' + access_token}
+    url = '/users/books'
+
+    resp = test_client_with_db.get(url, headers=headers)
+
+    assert resp.status_code == 200
+    assert isinstance(resp.json['data'], list)
+    assert len(resp.json['data']) == 2
+    assert resp.json['data'][0]['id'] == '1'
+    assert resp.json['data'][0]['title'] == 'The Wookiee Storybook'
+    assert resp.json['data'][0][
+        'description'] == 'The Wookiee Storybook description'
+    assert resp.json['data'][0]['author'] == 'CB'
+    assert resp.json['data'][0]['price'] == 30.0
+    assert resp.json['data'][0]['cover'] == 'image-cover-url-1'
+    assert 'publisher' not in resp.json['data'][0]
+    assert 'links' in resp.json['data'][0]
+    assert resp.json['data'][0]['links']['self'] == 'http://localhost/books/1'
+
+    assert resp.json['data'][1]['id'] == '2'
+    assert resp.json['data'][1][
+        'title'] == 'Wookiee Cookies: A Star Wars Cookbook'
+    assert resp.json['data'][1]['author'] == 'CB'
+    assert resp.json['data'][1]['price'] == 25.0
+    assert resp.json['data'][1]['cover'] == 'image-cover-url-2'
+    assert 'publisher' not in resp.json['data'][1]
+    assert 'links' in resp.json['data'][1]
+    assert resp.json['data'][1]['links']['self'] == 'http://localhost/books/2'
+
+
+def test_get_all_user_books_for_a_user_without_books(test_client_with_db):
+    """
+    GET /users/books
+    Get all books an authenticated user owns, in this case the user has no
+    books
+    """
+    # first with authenticate the user
+    url = '/auth'
+    mimetype = 'application/json'
+    headers = {'Content-Type': mimetype, 'Accept': mimetype}
+    data = {'username': 'obi-wan', 'password': 'Test1234'}
+    resp = test_client_with_db.post(url,
+                                    data=json.dumps(data),
+                                    headers=headers)
+
+    assert resp.content_type == mimetype
+    assert resp.status_code == 200
+    assert isinstance(resp.json['data'], dict)
+    assert 'access_token' in resp.json['data']
+    access_token = resp.json['data']['access_token']
+
+    # then get the books the authenticated user owns
+    headers = {'Authorization': 'Bearer ' + access_token}
+    url = '/users/books'
+
+    resp = test_client_with_db.get(url, headers=headers)
+
+    assert resp.status_code == 200
+    assert isinstance(resp.json['data'], list)
+    assert len(resp.json['data']) == 0
